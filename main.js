209e6236565221e28b30c2583a05bf9fdb06f47c
Rayen Chati <Rayenchatti0@gmail.com>
2026-01-05 13:49:11 +0100

    js update

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const button = document.querySelector("button")
+const input= document.querySelector("input")
+const cont=document.getElementById("cont")
+//console.log(button)
+button.addEventListener("click",(eo)=> {
+    eo.preventDefault()
+    const task = ` <div class="task">  
+
+<span class="icon-star-full icon"></span>
+
+<p> ${input.value} </p>
+<div>
+
+<span class="icon-bin icon"></span>
+<span class="icon-angry icon"></span>
+</div>
+
+<!--<span class="icon-heart"></span>-->
+    </div>
+    `
+    cont.innerHTML+=task
+
+});
 
