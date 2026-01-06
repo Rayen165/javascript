28f87274d8ac12f89e16da49ba2a9c8972249f2c
Rayen Chati <Rayenchatti0@gmail.com>
2026-01-06 14:29:09 +0100

    up

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const button = document.querySelector("button")
 const input= document.querySelector("input")
 const cont=document.getElementById("cont")
+cont.style.border= "3px solid red"
+cont.addEventListener("click", (eo) =>{
+   if(eo.target.className == "icon-bin icon") {
+    eo.target.parentElement.parentElement.remove()
+   }
+   else if(eo.target.className=="icon-angry icon"){
+    eo.target.classList.add("dn");
+    const heart =`<span class="icon-heart"></span>`
+
+    eo.target.parentElement.parentElement.getElementsByClassName("text")[0].classList.add("finish");
+
+   
+    eo.target.parentElement.innerHTML+= heart
+   }
+})
 //console.log(button)
 button.addEventListener("click",(eo)=> {
     eo.preventDefault()
